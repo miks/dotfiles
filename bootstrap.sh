ced40d05a8b4152ff65b3eacd5ba3d145df2ea9d
miks <miks@cubesystems.lv>
2015-05-12 21:01:13 +0300

    dotfiles bootstrap installation

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+########## Variables
+
+dir=~/code/dotfiles               # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files=".gitconfig .gitignore_global .pryrc .vimrc.after .vimrc.before .zshrc .my.cnf"   # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
